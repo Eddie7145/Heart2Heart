2c663df2bcfc46fb17a734f89343f418db3385ff
procode47 <iloenyenwavictor@gmail.com>
2023-02-09 22:25:26 +0100

    updated mobile view

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,9 @@
 var app = document.getElementById("app");
+const menu = document.getElementById("menu_btn");
+const close = document.getElementById("close");
+const overlay = document.getElementById("menu_overlay");
+const music_player = document.getElementById("player");
+const song = document.getElementById("song");
 
 window.addEventListener("DOMContentLoaded", (event) => {
   const audio = document.querySelector("audio");
@@ -20,3 +25,16 @@ typewriter
   .typeString("Live!")
   .pauseFor(2500)
   .start();
+
+  menu.addEventListener("click", () => {
+    overlay.classList.toggle("menu_open")
+  })
+close.addEventListener("click", () => {
+    overlay.classList.toggle("menu_open")
+})
+music_player.addEventListener("mouseover", () => {
+  song.classList.add("show")
+  })
+  music_player.addEventListener("mouseout", () => {
+  song.classList.remove("show")
+  })
